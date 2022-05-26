a5c284f4e70863a7b1edac069e1a68f90164132c
Lê Vũ Phước <82875240+phuoc-123-phuoc@users.noreply.github.com>
2022-05-26 21:40:44 +0700

    Update DanhSachKetQuaHocTap.java

--- Bai3_9/DanhSachKetQuaHocTap.java
+++ Bai3_9/DanhSachKetQuaHocTap.java
@@ -3,14 +3,14 @@ public class DanhSachKetQuaHocTap {
     private String tenHocKi;
     private float diemHocKi;
 
-    // Hàm tạo của lớp chương sách
+    // Hàm tạo của lớp danh sach ket qua hoc tap
     public DanhSachKetQuaHocTap(String tenHocKi, float diemHocKi)
     {
         this.tenHocKi=tenHocKi;
         this.diemHocKi=diemHocKi;
     }
-    // phương thức lấy tiêu đề của chương sách
+    // phương thức lấy ten hoc ki cua danh sach ket qua hoc tap
     public String gettenHocKi(){return tenHocKi;}
-    // phương thức lấy số trang của chương sách
+    // phương thức lấy điem hoc ki cua danh sach ket qua hoc tap
     public float getdiemHocKi(){return diemHocKi;}
 }
